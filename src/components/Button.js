d2e56afb72f0f4487d7c1f9931e51864289460c9
katsoulask <katsoulaskwstas@gmail.com>
2020-12-08 09:55:07 +0200

    Fragment remove

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,13 +4,13 @@ export default function Button (props) {
  
  
     return (
-      <>
+     
         <input
           type="button"
           value={props.value}
           onClick={(e)=>props.handleClick(e.target.value)}
         />
-      </>)
+    )
 }
 
 
